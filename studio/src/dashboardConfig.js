e50891187f6a0ba4456b312058b056811db9ea5c
Sanity.io <hello@sanity.io>
2021-10-27 13:16:15 -0700

    Initial commit

---
+++ studio/src/dashboardConfig.js
@@ -0,0 +1,57 @@
+export default {
+  widgets: [
+    { name: "structure-menu" },
+    {
+      name: "project-info",
+      options: {
+        __experimental_before: [
+          {
+            name: "netlify",
+            options: {
+              description:
+                "NOTE: Because these sites are static builds, they need to be re-deployed to see the changes when documents are published.",
+              sites: [
+                {
+                  buildHookId:
+                    "6179b38a5b17437862ebac44",
+                  title: "Sanity Studio",
+                  name: "sanity-gatsby-blog-contentsync-studio",
+                  apiId: "03f047c7-9851-42f3-8bbb-60a1b5d7c1cd",
+                },
+                {
+                  buildHookId: "6179b38a329fc6740555273c",
+                  title: "Blog Website",
+                  name: "sanity-gatsby-blog-contentsync",
+                  apiId: "26726480-c7d1-4d65-971f-4f772f0aa6cf",
+                },
+              ],
+            },
+          },
+        ],
+        data: [
+          {
+            title: "GitHub repo",
+            value:
+              "https://github.com/kathmbeck/sanity-gatsby-blog-contentsync",
+            category: "Code",
+          },
+          {
+            title: "Frontend",
+            value: "https://sanity-gatsby-blog-contentsync.netlify.app",
+            category: "apps",
+          },
+        ],
+      },
+    },
+    { name: "project-users", layout: { height: "auto" } },
+    {
+      name: "document-list",
+      options: {
+        title: "Recent blog posts",
+        order: "_createdAt desc",
+        types: ["post"],
+      },
+      layout: { width: "medium" },
+    },
+  ],
+};
